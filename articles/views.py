f65855cb313d5562783f5c3db7848b882ae8eae2
mechauk418 <mechauk418@gmail.com>
2023-10-06 12:17:22 +0900

    update article modify7

--- articles/views.py
+++ articles/views.py
@@ -9,10 +9,9 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Count
 from rest_framework.decorators import api_view
-from django.views.decorators.csrf import csrf_exempt
 
 
-@csrf_exempt
+@api_view(['POST'])
 def modify_article(request):
 
     if request.method == 'POST':
