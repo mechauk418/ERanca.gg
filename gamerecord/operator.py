5b52eeb06de24e107a66cf61cd86cac56a73226c
mechauk418 <mechauk418@gmail.com>
2023-10-10 13:49:20 +0900

    test oper05

--- gamerecord/operator.py
+++ gamerecord/operator.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
 from django_apscheduler.jobstores import register_events
-from .views import gainrp
+from .views import gainrp, gainrp2, gainrp3
 
 
 def start():
@@ -11,9 +11,21 @@ def start():
 
     scheduler.add_job(
         gainrp,
-        trigger=CronTrigger(day_of_week="tue", hour="13", minute="45"),
+        trigger=CronTrigger(day_of_week="tue", hour="14", minute="00"),
         max_instances=1,
         name="gainrp",
+    )
+    scheduler.add_job(
+        gainrp2,
+        trigger=CronTrigger(day_of_week="tue", hour="14", minute="30"),
+        max_instances=1,
+        name="gainrp2",
+    )
+    scheduler.add_job(
+        gainrp3,
+        trigger=CronTrigger(day_of_week="tue", hour="15", minute="00"),
+        max_instances=1,
+        name="gainrp3",
     )
 
     scheduler.start()
