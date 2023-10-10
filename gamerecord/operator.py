b1167a29d60268820315f61bd2eb968e4f7d4d25
mechauk418 <mechauk418@gmail.com>
2023-10-10 22:40:27 +0900

    op test01

--- gamerecord/operator.py
+++ gamerecord/operator.py
@@ -11,34 +11,36 @@ def start():
 
     scheduler.add_job(
         resetrp,
-        trigger=CronTrigger(day_of_week="tue", hour="20", minute="05"),
+        trigger=CronTrigger(day_of_week="tue", hour="22", minute="45"),
         max_instances=1,
         name="resetrp",
     )
 
     scheduler.add_job(
         gainrp(0,333),
-        trigger=CronTrigger(day_of_week="tue", hour="20", minute="10"),
+        trigger=CronTrigger(day_of_week="tue", hour="22", minute="50"),
         max_instances=1,
         name="gainrp",
     )
     scheduler.add_job(
         gainrp(333,666),
-        trigger=CronTrigger(day_of_week="tue", hour="20", minute="30"),
+        trigger=CronTrigger(day_of_week="tue", hour="23", minute="10"),
         max_instances=1,
         name="gainrp2",
     )
     scheduler.add_job(
         gainrp(666,1000),
-        trigger=CronTrigger(day_of_week="tue", hour="20", minute="50"),
+        trigger=CronTrigger(day_of_week="tue", hour="23", minute="30"),
         max_instances=1,
         name="gainrp3",
     )
     scheduler.add_job(
         rpeff,
-        trigger=CronTrigger(day_of_week="tue", hour="21", minute="10"),
+        trigger=CronTrigger(day_of_week="tue", hour="23", minute="50"),
         max_instances=1,
         name="rpeff",
     )
 
     scheduler.start()
+
+#test
