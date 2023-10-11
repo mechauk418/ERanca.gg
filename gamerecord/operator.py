dc2ce98fb883303ab066544f9ae1100314b729d2
mechauk418 <mechauk418@gmail.com>
2023-10-11 10:53:41 +0900

    test op08

--- gamerecord/operator.py
+++ gamerecord/operator.py
@@ -5,15 +5,6 @@ from django_apscheduler.jobstores import register_events, DjangoJobStore
 from .views import gainrp, resetrp, rpeff
 
 
-def testop():
-
-    print('testop')
-    resetrp()
-    gainrp(0,333)
-    gainrp(333,666)
-    gainrp(666,1000)
-    pass
-
 def start():
 
     scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
@@ -21,12 +12,42 @@ def start():
     register_events(scheduler)
 
     scheduler.add_job(
-        testop,
-        trigger=CronTrigger(day_of_week="wed", hour="10", minute="50"),
+        resetrp,
+        trigger=CronTrigger(day_of_week="wed", hour="11", minute="05"),
         id="resetrp",
         max_instances=1,
         replace_existing=True
     )
+    scheduler.add_job(
+        gainrp(0,333),
+        trigger=CronTrigger(day_of_week="wed", hour="11", minute="10"),
+        id="gainrp1",
+        max_instances=1,
+        replace_existing=True
+    )
+    scheduler.add_job(
+        gainrp(333,666),
+        trigger=CronTrigger(day_of_week="wed", hour="11", minute="30"),
+        id="gainrp2",
+        max_instances=1,
+        replace_existing=True
+    )
+    scheduler.add_job(
+        gainrp(666,1000),
+        trigger=CronTrigger(day_of_week="wed", hour="11", minute="50"),
+        id="gainrp3",
+        max_instances=1,
+        replace_existing=True
+    )
+    scheduler.add_job(
+        rpeff,
+        trigger=CronTrigger(day_of_week="wed", hour="12", minute="10"),
+        id="rpeff",
+        max_instances=1,
+        replace_existing=True
+    )
+
+
     scheduler.start()
 
 #test
