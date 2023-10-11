47ccd1f415c2f8c3832bc32c204aff77938c330c
mechauk418 <mechauk418@gmail.com>
2023-10-11 09:58:34 +0900

    test op06

--- gamerecord/operator.py
+++ gamerecord/operator.py
@@ -22,7 +22,7 @@ def start():
 
     scheduler.add_job(
         testop,
-        trigger=CronTrigger(day_of_week="wed", hour="09", minute="50"),
+        trigger=CronTrigger(day_of_week="wed", hour="10", minute="30"),
         max_instances=1,
         name="resetrp",
     )
